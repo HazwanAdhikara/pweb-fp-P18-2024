728880572df52a4d408dde41417f4d451d8df614
HazwanAdhikara <hazwanadh@gmail.com>
2024-12-16 22:13:06 +0700

    initialize project structure

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { connectToDatabase } from "./db-connection";
+
+const app = express();
+
+app.use(express.json());
+
+// check endpoint
+app.get("/", (_, response) => {
+  response.status(200).send("Server is up and running 💫");
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Express is running on Port ${PORT}`);
+});
+
+connectToDatabase();
